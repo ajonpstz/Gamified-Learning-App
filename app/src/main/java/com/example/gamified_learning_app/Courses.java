98f4b24a66117b7575f7681d0c16270d99a24a7a
Samuel Ardoin <samuelardoin@gmail.com>
2019-11-03 11:37:56 -0600

    Added courses activity

--- app/src/main/java/com/example/gamified_learning_app/Courses.java
+++ app/src/main/java/com/example/gamified_learning_app/Courses.java
@@ -6,26 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
-public class Homepage extends AppCompatActivity {
+public class Courses extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        setContentView(R.layout.activity_courses);
     }
-
 
     public void goToLogin(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void goToCourses(View view){
-        Intent intent = new Intent(this, Courses.class);
+    public void goToHomepage(View view){
+        Intent intent = new Intent(this, Homepage.class);
         startActivity(intent);
     }
-
-    //sam test
-
 }
