a119ee68e82002abb4aa430f9a80d3708a431eab
Samuel Ardoin <samuelardoin@gmail.com>
2019-11-30 13:21:41 -0600

    Cleaned Courses.java

--- app/src/main/java/com/example/gamified_learning_app/Courses.java
+++ app/src/main/java/com/example/gamified_learning_app/Courses.java
@@ -1,31 +1,20 @@
 package com.example.gamified_learning_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.example.gamified_learning_app.networkio.NetworkConstants;
-import com.github.nkzawa.emitter.Emitter;
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 
 public class Courses extends AppCompatActivity {
 
-    ImageView load_icon;
-    Animation rotate_anim;
     LinearLayout llayout;
 
     private FirebaseAuth mAuth;
@@ -33,66 +22,14 @@ public class Courses extends AppCompatActivity {
     ArrayList<TextView> textViews = new ArrayList<>();
 
 
-    private Socket socket;
-    {
-        try{
-            socket = IO.socket("http://" + NetworkConstants.SERVER_IP + ":" + NetworkConstants.SERVER_PORT + "/");
-            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
-                @Override
-                public void call(Object... args) {
-
-                }
-            }).on("Courses", new Emitter.Listener() {
-                @Override
-                public void call(Object... args) {
-                    if (args != null && args.length >= 1){
-                        runOnUiThread(new Runnable() {
-                            public void run() {
-                                load_icon.clearAnimation();
-                                llayout.removeView(load_icon);
-                                String[] courses = ((String)args[0]).split("\n");
-                                for (int i = 0 ; i < courses.length; i ++){
-                                    System.out.println(courses[i]);
-                                    TextView t = new TextView(getApplicationContext());
-                                    t.setText(courses[i]);
-                                    t.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorText));
-                                    t.setPadding(50,50,50,50);
-                                    t.setTextSize(20);
-                                    t.setBackgroundResource(R.drawable.text_border);
-                                    //t.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
-
-                                    llayout.addView(t);
-                                    textViews.add(t);
-                                }
-                            }
-                        });
-                    }
-                }
-            });
-        }catch(URISyntaxException e){
-            throw new RuntimeException(e);
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
-
         setContentView(R.layout.activity_courses);
-
-        load_icon = new ImageView(getApplicationContext());
-        load_icon.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        load_icon.setImageDrawable(getDrawable(R.drawable.icons8_loading));
-        load_icon.setPadding(0,200,0,200);
-
-
         llayout = (LinearLayout) findViewById(R.id.linearLayout);
-        llayout.addView(load_icon);
-        rotate_anim = AnimationUtils.loadAnimation(this,R.anim.rotate);
-        load_icon.startAnimation(rotate_anim);
 
-        socket.connect();
+
     }
 
     public void goToLogin(View view) {
@@ -101,7 +38,6 @@ public class Courses extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
-        socket.disconnect();
     }
 
     public void goToHomepage(View view){
@@ -109,6 +45,5 @@ public class Courses extends AppCompatActivity {
         startActivity(intent);
         overridePendingTransition(0, 0);
 
-        socket.disconnect();
     }
 }
